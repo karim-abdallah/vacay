31f8246d5bfc71d24084cbdb13d719fb8fb51838
Karim <ka2566@columbia.edu>
2022-07-19 20:58:48 +0200

    Adds styled package and logo header.

--- vacay/src/index.tsx
+++ vacay/src/index.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import styled from 'styled-components';
 
-class Game extends React.Component {
+class VacayHome extends React.Component {
   render() {
     return (
-      <div>
-        Welcome to Vacay
-      </div>);
+      <VacayBackground>
+        <VacayLogo>
+          VACAY
+        </VacayLogo>
+      </VacayBackground>);
   }
 }
+
+const VacayBackground = styled.div`
+    background: #F4F7FE;
+    position: relative;
+    width: 100%;
+    height: 100%;
+`;
+
+const VacayLogo = styled.div`
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 55px;
+    line-height: 100%;
+    /* identical to box height, or 55px */
+
+
+    color: #2B3674
+`;
 
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<VacayHome />);
