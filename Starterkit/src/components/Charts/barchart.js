10c5dbdee6f655ceed30f715b18336ff0305da07
Karim <ka2566@columbia.edu>
2022-09-16 17:01:28 -0400

    Adds mocks for API responses ahead of creating function that computes.

--- Starterkit/src/components/Charts/barchart.js
+++ Starterkit/src/components/Charts/barchart.js
@@ -1,13 +1,34 @@
 import React from "react"
 import { Bar } from "react-chartjs-2"
-import { mockData } from "../../mocks/dashboardSummary.mock"
+import { mockData, mockApiResponse, mockSelectedDates } from "../../mocks/dashboardSummary.mock"
 
-const generateDashboardData = () => {
+const generateDashboardData = (
+    currentMonth,
+    currentBalanceDays,
+    bookedPTO,
+    holidays,
+    accrualRate,
+    accrualCap,
+    selectedDates
+) => {
+    // Compute booked PTO per month
+    // compute holidays per month
+    // compute balance = previous balance + accrual rate - booked
+    // consider case of accrual Cap
+
+    // generate 12 months starting from today's month
+    // setup starting month balance.
+    // For each month:
+    //   calculate: previous balance + accrual rate - booked
+    //   set holidays
+    //   set booked
+    //   set balanced
+
     return mockData;
 }
 
 const BarChart = () => {
-    const data = generateDashboardData();
+    const data = generateDashboardData(mockApiResponse.currentMonth, mockApiResponse.currentBalanceDays, mockApiResponse.bookedPTO, mockApiResponse.holidays, mockApiResponse.accrualRate, mockApiResponse.accrualCap, mockSelectedDates.dates);
     const options = {
         plugins: {
             legend: {
--- Starterkit/src/mocks/dashboardSummary.mock.js
+++ Starterkit/src/mocks/dashboardSummary.mock.js
@@ -25,3 +25,22 @@ export const mockData = {
         },
     ]
 }
+
+export const mockApiResponse = {
+    currentMonth: "September",
+    currentBalanceDays: 5,
+    bookedPTO: {
+        dates: ["2022-09-04", "2022-09-05", "2022-09-06", "2022-11-22", "2022-12-20", "2022-12-21", "2022-12-22", "2022-12-23", "2022-12-24", "2022-12-25", "2023-02-15", "2023-05-20", "2023-05-21", "2023-05-22", "2023-07-05", "2023-07-05", "2023-08-08"]
+    },
+    holidays: {
+        dates: ["2022-09-03", "2022-10-26", "2022-11-25", "2022-11-26", "2022-12-25", "2022-12-31", "2023-01-01", "2023-01-02", "2023-04-12", "2023-05-19", "2023-07-04"],
+    },
+    accrualRate: 1.5,
+    accrualCap: 10,
+}
+
+
+// These are user-input for selected PTO on the reactive app
+export const mockSelectedDates = [
+    "2022-03-31", "2022-04-01"
+]
