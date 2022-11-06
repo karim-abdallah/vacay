73de1b63413aceacd4bb10f8b76f9b40a508c906
Karim <ka2566@columbia.edu>
2022-11-05 20:12:14 -0400

    Commits missing file.

--- Starterkit/src/pages/Dashboard/index.jsx
+++ Starterkit/src/pages/Dashboard/index.jsx
@@ -2,55 +2,11 @@
 import React, { Component } from "react";
 import { Container, Card, CardBody } from "reactstrap";
 import BarChart from "../../components/Charts/barchart";
-import Calendar from "react-calendar";
-import { differenceInCalendarDays } from "date-fns";
 import "../../styles/style.css";
 import { computeNextTwelveMonths } from "../../helpers/vacay_helpers";
 import { mockApiResponse } from "../../mocks/dashboardSummary.mock";
 import styled from "styled-components";
-import { useState } from "react";
-
-function MiniCalendar(props) {
-  // TODO: pull initial dates for the given month specifically.
-  const initialDates = [new Date("2022-11-12"), new Date("2022-10-13")];
-  // these dates states are generated locally. Which is what we wanted. But how to
-  // share them accross the application?
-  const [bookedDates, setDates] = useState(initialDates);
-  // pull the redux value for booked days for the given month
-  // should be an array of days. When the array gets updated, pull it again and recompute the component.
-  // When setting values, should update the redux store
-  // pull the redux value for holiday
-  const tileFormatting = ({ date, view }) => {
-    // Add class to tiles in month view only
-    if (view === "month") {
-      // Check if a date React-Calendar wants to check is on the list of dates to add class to
-      if (bookedDates.find((dDate) => isSameDay(dDate, date))) {
-        return "bookedDays";
-      }
-    }
-    return "inactiveDays";
-  };
-
-  const handleDateSelection = (valueRange) => {
-    bookedDates.push(valueRange[0]);
-    setDates([...bookedDates]);
-  };
-
-  return (
-    <Calendar
-      activeStartDate={props.startDate}
-      onChange={handleDateSelection}
-      defaultView="month"
-      showNeighboringMonth={null}
-      tileClassName={tileFormatting}
-      selectRange={true}
-      prevLabel={null}
-      prev2Label={null}
-      next2Label={null}
-      nextLabel={null}
-    />
-  );
-}
+import MiniCalendar from "../../components/Calendar/miniCalendar";
 
 class Dashboard extends Component {
   render() {
@@ -76,10 +32,6 @@ class Dashboard extends Component {
       </React.Fragment>
     );
   }
-}
-
-function isSameDay(a, b) {
-  return differenceInCalendarDays(a, b) === 0;
 }
 
 const CalendarContainer = styled.div`
