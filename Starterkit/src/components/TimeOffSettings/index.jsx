504ae792c49b5ce8b11129d54b2ec302c22ab7aa
Karim <ka2566@columbia.edu>
2022-12-01 18:34:54 -0500

    Adds some packages and completes scaffolding of settings modal.

--- Starterkit/src/components/TimeOffSettings/index.jsx
+++ Starterkit/src/components/TimeOffSettings/index.jsx
@@ -1,13 +1,50 @@
 import { Card, CardBody } from "reactstrap";
+import styled from "styled-components";
 
 const TimeOffSettings = () => {
   return (
     <Card>
       <CardBody>
-        <h1>Time Off Settings</h1>
+        <TimeOffSettingsContainer>
+          <div>
+            <TimeOffSettingsHeader>Time Off Settings</TimeOffSettingsHeader>
+            <MinimizeButton>Minimize</MinimizeButton>
+          </div>
+          <SettingsContainer>
+            <SettingsSubheader>Holidays</SettingsSubheader>
+            <SettingsSubheader>Options</SettingsSubheader>
+          </SettingsContainer>
+        </TimeOffSettingsContainer>
       </CardBody>
     </Card>
   );
 };
+
+const SettingsSubheader = styled.div`
+  font-size: 16px;
+  border-bottom: solid;
+`;
+
+const MinimizeButton = styled.button`
+  float: right;
+  font-size: 14px;
+  text-align: right;
+`;
+
+const TimeOffSettingsContainer = styled.div`
+  display: grid;
+`;
+
+const TimeOffSettingsHeader = styled.div`
+  float: left;
+  text-align: left;
+  font-size: 20px;
+`;
+
+const SettingsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 50px;
+`;
 
 export default TimeOffSettings;
