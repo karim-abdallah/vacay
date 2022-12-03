e072c48613b73c60b67c435f280e97d7dcb1b093
Karim <ka2566@columbia.edu>
2022-12-03 16:17:18 -0500

    Did a little more than the option column lol now the whole thing is setup but not wired to redux store.

--- Starterkit/src/components/TimeOffSettings/index.jsx
+++ Starterkit/src/components/TimeOffSettings/index.jsx
@@ -2,8 +2,67 @@ import { Card, CardBody } from "reactstrap";
 import styled from "styled-components";
 import { useState } from "react";
 
+function HolidayCheckbox(props) {
+  return (
+    <div>
+      <input type="checkbox" id={props.holiday} name={props.holiday} />
+      <label for={props.holiday}>{props.holiday}</label>
+    </div>
+  );
+}
+
+const HolidaysPane = () => {
+  const availableHolidays = [
+    "New Year's Day",
+    "Labor Day",
+    "Independence Day",
+    "Martin Luther King Day",
+    "Thanksgiving Day",
+    "Juneteenth",
+    "Memorial Day",
+    "Christmas Day",
+    "Veterans Day",
+  ];
+
+  const holidayCheckboxes = availableHolidays.map((item, index) => {
+    return <HolidayCheckbox holiday={item} />;
+  });
+
+  return (
+    <PaneContainer>
+      <SettingsSubheader>Holidays</SettingsSubheader>
+      <HolidaysContainer>
+        {holidayCheckboxes}
+        <button>Add Holiday</button>
+      </HolidaysContainer>
+    </PaneContainer>
+  );
+};
+
+const OptionsPane = () => {
+  return (
+    <PaneContainer>
+      <SettingsSubheader>Options</SettingsSubheader>
+      <OptionsContainer>
+        <div>Days</div>
+        <div>Vacation Time</div>
+        <div>Personnal Time (PTO)</div>
+        <div>Annual Allowance</div>
+        <input type="number" />
+        <input type="number" />
+        <div>Annual Cap</div>
+        <input type="number" />
+        <input type="number" />
+        <div>Current Balance (September)</div>
+        <input type="number" />
+        <input type="number" />
+      </OptionsContainer>
+    </PaneContainer>
+  );
+};
+
 const TimeOffSettings = () => {
-  const [expandedSettings, setExpandedSettings] = useState(true);
+  const [expandedSettings, setExpandedSettings] = useState(false);
 
   const handleExpandSettings = () => {
     console.log("Expand Settings");
@@ -29,8 +88,8 @@ const TimeOffSettings = () => {
                 </MinimizeButton>
               </div>
               <SettingsContainer>
-                <SettingsSubheader>Holidays</SettingsSubheader>
-                <SettingsSubheader>Options</SettingsSubheader>
+                <HolidaysPane />
+                <OptionsPane />
               </SettingsContainer>
             </TimeOffSettingsContainer>
           </CardBody>
@@ -39,6 +98,20 @@ const TimeOffSettings = () => {
     </>
   );
 };
+
+const PaneContainer = styled.div`
+  display: grid;
+`;
+
+const HolidaysContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const OptionsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
 
 const ExpandSettingsCard = styled(Card)`
   text-align: right;
