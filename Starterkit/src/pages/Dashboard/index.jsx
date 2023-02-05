c56c760e177e1486bbd2a61d9ee2f02f7718141e
Karim <ka2566@columbia.edu>
2023-02-04 22:35:29 -0500

    Removes unused imports.

--- Starterkit/src/pages/Dashboard/index.jsx
+++ Starterkit/src/pages/Dashboard/index.jsx
@@ -9,12 +9,10 @@ import {
 } from "../../store/dashboard/selector";
 import BarChart from "../../components/Charts/barchart";
 import "../../styles/style.css";
-import { bookedPtoColor, unbookColor } from "../../styles/constants";
 import { computeNextTwelveMonths } from "../../helpers/vacay_helpers";
 import styled from "styled-components";
 import MiniCalendar from "../../components/Calendar/miniCalendar";
 import TimeOffSettings from "../../components/TimeOffSettings/index";
-import expand from "../../assets/images/expand.png";
 
 const Dashboard = () => {
   const currentMonth = useSelector(getCurrentMonth);
@@ -81,31 +79,6 @@ const Dashboard = () => {
 
 const StyledAlert = styled(Alert)`
   margin-bottom: 10px;
-`;
-
-const MockStyledButton = styled.b`
-  margin-bottom: 10px;
-  margin-left: 5px;
-  margin-right: 5px;
-  padding: 3px 23px 3px;
-  color: #ffffff;
-  border-radius: 25px;
-  font-size: 13px;
-`;
-
-const MockStyledExpandButton = styled.img`
-  background-color: #e9e6f6;
-  border-radius: 5px;
-  height: 22px;
-  padding: 5px;
-`;
-
-const MockBookButton = styled(MockStyledButton)`
-  background-color: ${bookedPtoColor};
-`;
-
-const MockUnbookButton = styled(MockStyledButton)`
-  background-color: ${unbookColor};
 `;
 
 const GroupDiv = styled.div`
