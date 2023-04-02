5871542547378002250405913623a5293dfb97b0
Karim <ka2566@columbia.edu>
2023-04-01 20:00:48 -0400

    Adds card for group container.

--- Starterkit/src/pages/PlanWithFriends/index.jsx
+++ Starterkit/src/pages/PlanWithFriends/index.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { Container, Card, CardBody } from "reactstrap";
+import styled from "styled-components";
 
 const PlanWithFriends = () => {
   return (
     <React.Fragment>
       <div className="page-content">
-        <h2>COMING SOON</h2>
+        <Container fluid>
+          <h2>Plan with friends - COMING SOON</h2>
+          <Card>
+            <StyledCardBody>
+              <div>Friend component</div>
+              <div>Calendar component</div>
+            </StyledCardBody>
+          </Card>
+        </Container>
       </div>
     </React.Fragment>
   );
 };
+
+const StyledCardBody = styled(CardBody)`
+  display: flex;
+`;
 
 export default PlanWithFriends;
