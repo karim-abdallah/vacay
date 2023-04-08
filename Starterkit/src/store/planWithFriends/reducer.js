3aba8987a14fd2551e1857853059b3ed8c9dcdc1
Karim <ka2566@columbia.edu>
2023-04-07 20:19:31 -0400

    Adds reducer and mock structures.

---
+++ Starterkit/src/store/planWithFriends/reducer.js
@@ -0,0 +1,26 @@
+const INIT_STATE = {
+  groups: []
+};
+
+const PlanWithFriends = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case "planWithFriends/addFriend":
+      // Takes in a group id and a friend object to add to a group
+      return {
+        ...state,
+        groups: state.groups.map((item, index) => {
+          if (item.groupId !== action.groupId) {
+            return item;
+          } else {
+            const updatedGroup = {
+              ...item,
+              friends: [...item.friends, action.friend]
+            };
+            return updatedGroup;
+          }
+        })
+      };
+    default:
+      return state;
+  }
+};
