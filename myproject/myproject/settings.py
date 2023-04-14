927b42449d99548db26a0ababe54ad2e5c15bbd0
Aliasgher Nooruddin <aliasghernooruddin@gmail.com>
2023-04-14 21:59:21 +0500

    project setup done

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +79,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #    'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'vacay',
+        'PASSWORD': 'Vacaydbadmin2023!',
+        'HOST': 'vacay-db.cwx5iz62mons.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
