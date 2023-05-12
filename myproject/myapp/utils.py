43282e2f4b7d9c6dd5e45dbf75a59294f7a30f5c
Karim <ka2566@columbia.edu>
2023-05-12 12:17:22 -0400

    fixes up boto config.

--- myproject/myapp/utils.py
+++ myproject/myapp/utils.py
@@ -2,86 +2,79 @@ import boto3
 from .templates import FORGET_PASSWORD_TEMPLATE, REGISTER_USER_TEMPLATE
 from .models import User
 
-ACCESS_KEY = 'AKIAVH77JYOVXV547FMB'
-SECRET_ACCESS_KEY = '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'
-
 ses_client = boto3.client(
-    'ses',
-    region_name='us-east-1',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_ACCESS_KEY
+    "ses",
+    region_name="us-east-1",
 )
 
 s3_client = boto3.client(
-    's3',
-    region_name='us-east-1',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_ACCESS_KEY
+    "s3",
+    region_name="us-east-1",
 )
 
-S3_BUCKET = 'vacay-assets'
+S3_BUCKET = "vacay-assets"
 
 
 def send_forget_password_email(recepient, link):
 
     template = FORGET_PASSWORD_TEMPLATE
-    content = template.replace('reset_password_link', link)
+    content = template.replace("reset_password_link", link)
 
-    send_email(recepient, content, 'Forget Password Link')
+    send_email(recepient, content, "Forget Password Link")
 
 
 def send_register_user_email(recepient, first_name):
 
     template = REGISTER_USER_TEMPLATE
-    content = template.replace('first_name', first_name)
+    content = template.replace("first_name", first_name)
 
-    send_email(recepient, content, 'Welcome to Vacay!')
+    send_email(recepient, content, "Welcome to Vacay!")
 
 
 def send_email(receipient, content, subject):
     response = ses_client.send_email(
         Destination={
-            'ToAddresses': [receipient],
-             'CcAddresses': [
-            'aliasghernooruddin@gmail.com', 'info@vacay.live'
-        ],
+            "ToAddresses": [receipient],
+            "CcAddresses": ["aliasghernooruddin@gmail.com", "info@vacay.live"],
         },
         Message={
-            'Body': {
-                'Html': {
-                    'Charset': 'UTF-8',
-                    'Data': content,
+            "Body": {
+                "Html": {
+                    "Charset": "UTF-8",
+                    "Data": content,
                 },
             },
-            'Subject': {
-                'Charset': 'UTF-8',
-                'Data': subject,
+            "Subject": {
+                "Charset": "UTF-8",
+                "Data": subject,
             },
         },
-        Source='info@vacay.live',
+        Source="info@vacay.live",
     )
 
     return response
 
 
 def generate_presigned_url(username, file_name, file_type):
-    path = f'users/{username}/profile/{file_name}'
+    path = f"users/{username}/profile/{file_name}"
 
-    url = s3_client.generate_presigned_url('put_object',
-                                           Params={
-                                               'Bucket': S3_BUCKET,
-                                               'Key': path,
-                                               'ContentType': file_type,
-                                           },
-                                           ExpiresIn=360)
+    url = s3_client.generate_presigned_url(
+        "put_object",
+        Params={
+            "Bucket": S3_BUCKET,
+            "Key": path,
+            "ContentType": file_type,
+        },
+        ExpiresIn=360,
+    )
     return url
 
 
 def check_or_create_username(email):
-    username = email.split('@')[0]
+    username = email.split("@")[0]
     count = User.objects.filter(username=username).count()
 
     if count > 0:
-        username = username + str(count+1)
+        username = username + str(count + 1)
 
     return username
