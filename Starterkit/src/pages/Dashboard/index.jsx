19b4c2be573efb16010a7ed2c0d3445fe7985fce
Aliasgher Nooruddin <aliasghernooruddin@gmail.com>
2023-05-23 12:17:54 +0500

    ignore migrations

--- Starterkit/src/pages/Dashboard/index.jsx
+++ Starterkit/src/pages/Dashboard/index.jsx
@@ -50,6 +50,7 @@ const Dashboard = () => {
       let date = new Date(days.date);
       booked_dates.push(date);
     });
+     
     
     dispatch({ type: "bookedPTO/add", payload: [...booked_dates] });
     dispatch({ type: "settings/update", payload: PTOSettings });
