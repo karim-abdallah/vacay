db265b02259360ef09122325473481a77a7306bf
Karim <ka2566@columbia.edu>
2023-05-31 11:18:19 +0200

    Integrate pulling user data into plan with friends.

--- Starterkit/src/pages/PlanWithFriends/index.jsx
+++ Starterkit/src/pages/PlanWithFriends/index.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Card, CardBody } from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { GroupCard } from '../../components/Calendar/planWithFriendsCalendar';
 import { selectDashboardData } from '../../store/dashboard/selector';
 import { selectGroupInfo } from '../../store/planWithFriends/selectors';
+import { get } from '../../helpers/api_helper';
 import { selectProfileData } from '../../store/profile/selector';
 
 const nMonthsAheadDefault = 6;
 
 const PlanWithFriends = () => {
-  const myProfile = useSelector(selectProfileData);
+  const [myProfile, setMyProfile] = useState({});
+
+  const fetchMyProfile = async () => {
+    let data = await get('/dashboard/user');
+    setMyProfile({
+      username: data['username'],
+      email: data['email'],
+      name: `${data['first_name']} ${data['last_name']}`,
+      profilePic: data['profile_pic'],
+    });
+  };
+
   const myDashboardData = useSelector(selectDashboardData);
   const groupData = useSelector(selectGroupInfo);
+
+  useEffect(() => {
+    fetchMyProfile();
+  }, []);
 
   return (
     <React.Fragment>
