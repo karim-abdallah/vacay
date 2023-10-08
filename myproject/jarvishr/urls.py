86592c9fea1e4ca18a3a6cb17e46613ccb9073ec
Aliasgher Nooruddin <aliasghernooruddin@gmail.com>
2023-10-08 16:59:20 +0500

    create CRURD APIs to support google sheet crud operations

--- myproject/jarvishr/urls.py
+++ myproject/jarvishr/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("send-prompt", ChatBotView.as_view()),
     path("metrics", MetricsView.as_view()),
+    path("sheet-data", SheetView.as_view())
 ]
--- myproject/jarvishr/views.py
+++ myproject/jarvishr/views.py
@@ -9,6 +9,7 @@ from authentication.models import Metric
 from .constants import OPENAI_KEY
 from .serializers import MetricsSerializer
 from .utils import fetchGoogleSheet
+from .spreadsheet import SpreadSheet
 
 openai.api_key = OPENAI_KEY
 
@@ -53,3 +54,56 @@ class MetricsView(APIView):
         metrics = Metric.objects.all()
         serializer = MetricsSerializer(metrics, many=True)
         return Response(serializer.data)
+
+
+class SheetView(APIView):
+
+    permission_classes = [IsAuthenticated]
+    
+    spreadsheet_id = "1TxRItA8SPvJ_VkKDxTK6WL5vTmfPMzitGASw6Vrh-b0"
+    sheet_title = "Employee_Database"
+    
+    sheet = SpreadSheet(spreadsheet_id,sheet_title)
+    
+    def get(self,request):
+        try: 
+            spreadsheet_data = self.sheet.get_data()
+            return Response(spreadsheet_data,status=status.HTTP_200_OK) 
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+
+    def post(self,request):
+        try: 
+            rows_data = request.data
+            if rows_data and isinstance(rows_data, list):
+                resp = self.sheet.append_row(rows_data)
+                return Response({"message":resp},status=status.HTTP_200_OK) 
+            return Response({"error":"invalid data"},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+
+    def patch(self,request):
+        try: 
+            rows_data = request.data
+            if rows_data and isinstance(rows_data, list):
+                resp = self.sheet.edit_row(rows_data)
+                return Response({"message":resp},status=status.HTTP_200_OK) 
+            return Response({"error":"invalid data"},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+    def delete(self,request):
+        try: 
+            rows_data = request.data
+            if rows_data and isinstance(rows_data, list):
+                resp = self.sheet.delete_rows_by_ids(rows_data)
+                return Response({"message":resp},status=status.HTTP_200_OK) 
+            return Response({"error":"invalid data"},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
